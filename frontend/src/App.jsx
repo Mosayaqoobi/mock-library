989314af7dab0342a5e9c13bbcd3eb9251b49e90
Mosayaqoobi <yaqoobi@ualberta.ca>
2025-10-22 16:56:46 -0600

    Login functionality complete

---
+++ frontend/src/App.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import axios from "axios";
+
+
+function App() {
+  const [user,  setUser] = useState(null);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const fetchUser = async () => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      try {
+        const response = await axios.get("/api/users/me",{
+          headers: {Authorization: `Bearer ${token}`}
+      }) 
+    } catch (error) {
+        setError("failed to fetch user data");
+        localStorage.removeItem("token")
+      }
+    }
+    };
+    fetchUser();
+
+}, [])
+
+  return (
+    <Router>
+      <NavBar/>
+        <Routes>
+          <Route path = "/" element={<Home />} />
+          <Route path = "/login" element={<Login setUser = {setUser} />} />
+          <Route path = "/register" element={<Register />} />
+        </Routes>
+    </Router>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,75 @@
+import * as React from 'react';
+import { Box, TextField, Button, Typography, Paper, Alert } from '@mui/material';
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+export default function Login({setUser}) {
+  const [formData, setFormData ] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  //keep the value the same as what it was upon submiting
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.name]: e.target.value})
+  }
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("api/auth/login", formData);
+      localStorage.setItem("token", res.data.token);
+      setUser(res.data);
+      navigate("/");
+      console.log(res.data);
+
+    } catch (error) {
+      setError(error.response?.data?.message || "Login failed");
+    }
+  };
+  return (
+    <Box
+      sx={{
+        minHeight: 'calc(100vh - 64px)',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      {error && ( 
+        <Alert severity="error" sx={{ mb: 2, maxWidth: 400, width: '90%' }} onClose={() => setError("")}>
+          {error}
+        </Alert>
+      )}
+      <Paper elevation={2} sx={{p: 3,width: 400, backgroundColor: '#2c2c2c', maxWidth: '90%', boxShadow: '0 8px 32px rgba(0, 0, 0, 0.6)',}}>
+        <Typography variant="h5" align="center" sx={{ mb: 3, color: "white" }}>
+          Login
+        </Typography>
+        <form onSubmit={handleSubmit}>
+
+        <TextField label="Email" type="email" name="email" value={formData.email} onChange={handleChange} fullWidth margin="normal" InputLabelProps={{ style: { color: 'white' } }} InputProps={{ style: { color: 'white' } }}/>
+
+        <TextField label="Password" type="password" name = "password" value = {formData.password} onChange={handleChange} fullWidth margin="normal" InputLabelProps={{ style: { color: 'white' } }} InputProps={{ style: { color: 'white' } }}/>
+
+        <Button type = "submit" variant="contained" fullWidth sx={{ mt: 2, color: "white" }}>
+          Login
+        </Button>
+        </form>
+
+        <Typography variant="body2" align="center" sx={{ mt: 2 , color: "white"}}>
+          Don't have an account?{' '}
+          <Typography
+            component="span"
+            color="primary"
+            sx={{ cursor: 'pointer', fontWeight: 'bold' }}
+          >
+            Sign Up
+          </Typography>
+        </Typography>
+      </Paper>
+    </Box>
+  );
+}
